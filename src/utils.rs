473bf54bf4c3928a099327b8b360ecf10a335788
zuramai <saugi.dev@gmail.com>
2023-10-17 12:23:19 +0700

    ListNode type casting

--- src/utils.rs
+++ src/utils.rs
@@ -13,4 +13,32 @@ impl ListNode {
     }
   }
 
+  
+}
+
+impl From<Box<ListNode>> for Vec<i32> {
+  fn from(mut value: Box<ListNode>) -> Self {
+    let mut value = Some(&value);
+    let mut v: Vec<i32> = vec![];
+
+    while let Some(next) = value {
+      v.push(next.val);
+      value = next.next.as_ref()
+    }
+
+    v
+  }
+}
+impl From<Vec<i32>> for Box<ListNode> {
+  fn from(mut value: Vec<i32>) -> Self {
+    value.reverse();
+    let mut tail: Option<Box<ListNode>> = None;
+    for v in value {
+      tail = Some(Box::new(ListNode {
+        val: v,
+        next: tail,
+      }));
+    }
+    tail.unwrap()
+  }
 }

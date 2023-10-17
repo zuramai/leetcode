e40d5914a538b4214b968f49cfaf125572c6a36b
zuramai <saugi.dev@gmail.com>
2023-10-17 18:48:53 +0700

    update s0048 ptr swap

--- src/s0048_rotate_image.rs
+++ src/s0048_rotate_image.rs
@@ -1,4 +1,5 @@
-use std::mem;
+use std::ptr;
+
 
 struct Solution {}
 
@@ -6,9 +7,13 @@ impl Solution {
     pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
         for i in 0..matrix.len() {
             for j in i..matrix.len() {
-                let temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+                unsafe {
+                    ptr::swap(&mut matrix[i][j], &mut matrix[j][i]);
+                }
+                // Replace var
+                // let temp = matrix[i][j];
+                // matrix[i][j] = matrix[j][i];
+                // matrix[j][i] = temp;
             }
         }
         matrix.iter_mut().for_each(|v| v.reverse());

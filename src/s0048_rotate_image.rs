448c8aab9a4dbe900332f0c0894283b538fc6258
zuramai <saugi.dev@gmail.com>
2023-10-17 14:34:06 +0700

    s0048

---
+++ src/s0048_rotate_image.rs
@@ -0,0 +1,29 @@
+use std::mem;
+
+struct Solution {}
+
+impl Solution {
+    pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+        for i in 0..matrix.len() {
+            for j in i..matrix.len() {
+                let temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        matrix.iter_mut().for_each(|v| v.reverse());
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn test() {
+        assert_eq!(
+            Solution::rotate(&mut vec![vec![1,2,3],vec![4,5,6],vec![7,8,9]]),
+            Solution::rotate(&mut vec![vec![7,4,1],vec![8,5,2],vec![9,6,3]]),
+        );
+    }
+}
